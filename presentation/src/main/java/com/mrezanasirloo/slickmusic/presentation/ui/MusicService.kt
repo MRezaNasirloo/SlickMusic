5b047f85c00d32006c02c98c3ce80f3fbb3c6627
M. Reza Nasirloo <M.Reza.Nasirloo@gmail.com>
2018-07-04 13:11:58 +0430

    Implements more play back callbacks.

--- presentation/src/main/java/com/mrezanasirloo/slickmusic/presentation/ui/MusicService.kt
+++ presentation/src/main/java/com/mrezanasirloo/slickmusic/presentation/ui/MusicService.kt
@@ -3,7 +3,6 @@ package com.mrezanasirloo.slickmusic.presentation.ui
 import android.content.Intent
 import android.media.AudioManager
 import android.media.MediaPlayer
-import android.net.Uri
 import android.os.Bundle
 import android.support.v4.media.MediaBrowserCompat
 import android.support.v4.media.MediaBrowserServiceCompat
@@ -14,10 +13,9 @@ import android.support.v4.media.session.MediaSessionCompat.FLAG_HANDLES_MEDIA_BU
 import android.support.v4.media.session.MediaSessionCompat.FLAG_HANDLES_TRANSPORT_CONTROLS
 import android.support.v4.media.session.PlaybackStateCompat
 import android.support.v4.media.session.PlaybackStateCompat.*
+import com.mrezanasirloo.domain.implementation.model.Song
 import com.mrezanasirloo.slickmusic.R
-import com.mrezanasirloo.slickmusic.presentation.ui.song.model.Song
 import java.util.*
-import java.util.concurrent.TimeUnit
 
 
 class MusicService : MediaBrowserServiceCompat() {
@@ -28,6 +26,8 @@ class MusicService : MediaBrowserServiceCompat() {
 
     private val mMediaSession: MediaSessionCompat? = null
     private val stateBuilder: PlaybackStateCompat.Builder = PlaybackStateCompat.Builder()
+    private val metaDataBuilder: MediaMetadataCompat.Builder = MediaMetadataCompat.Builder()
+
     private var mediaPlayer: MediaPlayer? = null
     private lateinit var mediaSession: MediaSessionCompat
 
@@ -88,19 +88,31 @@ class MusicService : MediaBrowserServiceCompat() {
             }
         }
 
-        override fun onRewind() {
-            super.onRewind()
-            println("MusicService.onRewind")
-        }
-
         override fun onSeekTo(pos: Long) {
             super.onSeekTo(pos)
             println("MusicService.onSeekTo")
+            TODO("Implement this")
+        }
+
+        override fun onSkipToQueueItem(id: Long) {
+            super.onSkipToQueueItem(id)
+            playlist.filter { it.queueId == id }
+                    .map { queueIndex = playlist.indexOf(it) }
+            onPlay()
+        }
+
+        override fun onSkipToNext() {
+            super.onSkipToNext()
+            println("MusicService.onSkipToNext")
+            queueIndex++
+            onPlay()
         }
 
         override fun onSkipToPrevious() {
             super.onSkipToPrevious()
             println("MusicService.onSkipToPrevious")
+            queueIndex--
+            onPlay()
         }
 
         override fun onPlayFromMediaId(mediaId: String?, extras: Bundle?) {
@@ -114,8 +126,8 @@ class MusicService : MediaBrowserServiceCompat() {
                 if (!mediaSession.isActive) {
                     mediaSession.isActive = true
                 }
-                    mediaPlayer?.reset()
                 mediaPlayer?.run {
+                    reset()
                     setAudioStreamType(AudioManager.STREAM_MUSIC)
                     setDataSource(song.data)
                     prepare()
@@ -127,58 +139,43 @@ class MusicService : MediaBrowserServiceCompat() {
 
         }
 
-        override fun onPrepare() {
-            super.onPrepare()
-            println("MusicService.onPrepare")
-            if (queueIndex < 0 && playlist.isEmpty()) {
-                // Nothing to play.
-                return
-            }
-
-            val mediaUri = playlist[queueIndex].description.mediaUri
-/*
-            val metaData = MediaMetadataCompat.Builder()
-                    .putString(MediaMetadataCompat.METADATA_KEY_ARTIST, song.artistName)
-                    .putString(MediaMetadataCompat.METADATA_KEY_ALBUM_ARTIST, song.artistName)
-                    .putString(MediaMetadataCompat.METADATA_KEY_ALBUM, song.albumName)
-                    .putString(MediaMetadataCompat.METADATA_KEY_TITLE, song.title)
-                    .putLong(MediaMetadataCompat.METADATA_KEY_DURATION, song.duration)
-                    .putLong(MediaMetadataCompat.METADATA_KEY_TRACK_NUMBER, (getPosition() + 1).toLong())
-                    .putLong(MediaMetadataCompat.METADATA_KEY_YEAR, song.year)
-                    .putBitmap(MediaMetadataCompat.METADATA_KEY_ALBUM_ART, null)
-*/
-
-//                mPreparedMedia = MusicLibrary.getMetadata(this@MusicService, mediaId)
-//                mediaSession.setMetadata(mPreparedMedia)
-
-            if (!mediaSession.isActive) {
-                mediaSession.isActive = true
-            }
-        }
-
         override fun onPlay() {
             super.onPlay()
             println("MusicService.onPlay")
-/*
             if (playlist.isEmpty()) {
                 // Nothing to play.
                 return
             }
-*/
 
-/*
-            if (mPreparedMedia == null) {
-                onPrepare()
+            playlist[queueIndex].description.extras?.apply {
+                classLoader = this@MusicService.classLoader
+                val song = getParcelable<Song>("SONG")
+                if (mediaPlayer == null) {
+                    mediaPlayer = MediaPlayer()
+                }
+                if (!mediaSession.isActive) {
+                    mediaSession.isActive = true
+                }
+                mediaPlayer?.run {
+                    reset()
+                    setAudioStreamType(AudioManager.STREAM_MUSIC)
+                    setDataSource(song.data)
+                    prepare()
+                    start()
+                    mediaSession.setPlaybackState(stateBuilder.setState(STATE_PLAYING, mediaPlayer?.currentPosition?.toLong()!!, 1f).build())
+
+                    metaDataBuilder.putString(MediaMetadataCompat.METADATA_KEY_ARTIST, song.artistName)
+                            .putString(MediaMetadataCompat.METADATA_KEY_ALBUM_ARTIST, song.artistName)
+                            .putString(MediaMetadataCompat.METADATA_KEY_ALBUM, song.albumName)
+                            .putString(MediaMetadataCompat.METADATA_KEY_TITLE, song.title)
+                            .putLong(MediaMetadataCompat.METADATA_KEY_DURATION, song.duration)
+                            .putLong(MediaMetadataCompat.METADATA_KEY_TRACK_NUMBER, (queueIndex + 1).toLong())
+                            .putLong(MediaMetadataCompat.METADATA_KEY_YEAR, song.year.toLong())
+                            .putBitmap(MediaMetadataCompat.METADATA_KEY_ALBUM_ART, null)
+
+                    mediaSession.setMetadata(metaDataBuilder.build())
+                }
             }
-*/
-
-            mediaPlayer?.run {
-//                setDataSource(applicationContext, playlist[queueIndex].description.mediaUri)
-//                prepare()
-                start()
-                mediaSession.setPlaybackState(stateBuilder.setState(STATE_PLAYING, mediaPlayer?.currentPosition?.toLong()!!, 1f).build())
-            }
-
         }
 
         override fun onStop() {
@@ -193,11 +190,6 @@ class MusicService : MediaBrowserServiceCompat() {
 
         }
 
-        override fun onSkipToNext() {
-            super.onSkipToNext()
-            println("MusicService.onSkipToNext")
-        }
-
         override fun onPause() {
             super.onPause()
             println("MusicService.onPause")
@@ -206,29 +198,5 @@ class MusicService : MediaBrowserServiceCompat() {
             }
             mediaSession.setPlaybackState(stateBuilder.setState(STATE_PAUSED, mediaPlayer?.currentPosition?.toLong()!!, 1f).build())
         }
-    }
-
-    private fun createMediaMetadataCompat(
-            mediaId: String,
-            title: String,
-            artist: String,
-            album: String,
-            genre: String,
-            duration: Long,
-            durationUnit: TimeUnit,
-            musicFilename: String,
-            albumArtResId: Int,
-            albumArtResName: String): MediaMetadataCompat {
-        return MediaMetadataCompat.Builder()
-                .putString(MediaMetadataCompat.METADATA_KEY_MEDIA_ID, mediaId)
-                .putString(MediaMetadataCompat.METADATA_KEY_ALBUM, album)
-                .putString(MediaMetadataCompat.METADATA_KEY_ARTIST, artist)
-                .putLong(MediaMetadataCompat.METADATA_KEY_DURATION,
-                        TimeUnit.MILLISECONDS.convert(duration, durationUnit))
-                .putString(MediaMetadataCompat.METADATA_KEY_GENRE, genre)
-                .putString(MediaMetadataCompat.METADATA_KEY_ALBUM_ART_URI, null)
-                .putString(MediaMetadataCompat.METADATA_KEY_DISPLAY_ICON_URI, null)
-                .putString(MediaMetadataCompat.METADATA_KEY_TITLE, title)
-                .build()
     }
 }
